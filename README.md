9c137b2fc1784451c718a3573e2dd8d9d0d1a62b
Maxim P <maxxxlim@mail.ru>
2020-03-09 22:10:48 +0300

    add code climat

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 [![asciicast](https://asciinema.org/a/s2Cu8m36fLAw35TLGnHUcTpjG.svg)](https://asciinema.org/a/s2Cu8m36fLAw35TLGnHUcTpjG)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/b7bcfe5afa1f76f97e35/maintainability)](https://codeclimate.com/github/PolyMaG/python-project-lvl2/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/b7bcfe5afa1f76f97e35/test_coverage)](https://codeclimate.com/github/PolyMaG/python-project-lvl2/test_coverage)
--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,15 +1,15 @@
 def generate_diff(first_file, second_file):
     result = []
     for k, v in first_file.items() & second_file.items():
-      result.append('  ' + k + ': ' + str(v))
+        result.append('  ' + k + ': ' + str(v))
     for k in first_file.keys() & second_file.keys():
-      if first_file.get(k) != second_file.get(k):
-        result.append('+ ' + k + ': ' + str(second_file.get(k)))
-        result.append('- ' + k + ': ' + str(first_file.get(k)))
+        if first_file.get(k) != second_file.get(k):
+            result.append('+ ' + k + ': ' + str(second_file.get(k)))
+            result.append('- ' + k + ': ' + str(first_file.get(k)))
     for k in first_file.keys() - second_file.keys():
-      result.append('- ' + k + ': ' + first_file.get(k))
+        result.append('- ' + k + ': ' + first_file.get(k))
     for k in second_file.keys() - first_file.keys():
-      result.append('+ ' + k + ': ' + str(second_file.get(k)))
+        result.append('+ ' + k + ': ' + str(second_file.get(k)))
 
-    result ='{\n' + '\n'.join(result) + '\n}'
+    result = '{\n' + '\n'.join(result) + '\n}'
     return result
