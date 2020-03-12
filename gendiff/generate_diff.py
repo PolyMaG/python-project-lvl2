b785ee6fcd4641092de149275538d300f29ad92a
Maxim P <maxxxlim@mail.ru>
2020-03-12 23:13:13 +0300

    change get_path

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,9 @@
+import json
+
+
 def get_paths(first_file, second_file):
-    import os
-    import json
-    abspath_to_first_file = os.path.abspath(first_file)
-    abspath_to_second_file = os.path.abspath(second_file)
-    first_file = json.load(open(abspath_to_first_file))
-    second_file = json.load(open(abspath_to_second_file))
+    first_file = json.load(open(first_file))
+    second_file = json.load(open(second_file))
     return first_file, second_file
 
 
